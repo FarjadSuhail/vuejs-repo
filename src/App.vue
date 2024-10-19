0bfecc3081bb147995ad4f5b5ba1f09805ebd0a0
lenovo <farjadsuhail149@gmail.com>
2024-10-19 14:24:35 +0200

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    Hello {{ name }}
+    <div v-text="channel"></div>
+    <div v-html="channel"></div>
+    <h2 v-bind="heading">Heading</h2>
+    <button v-bind:disabled="isDisabled">Bind</button>
+
+    <h2 class="underline">Underline</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="{ new: isSoldOut, 'sold-out': !isSoldOut }">
+      SoldOut? Movie
+    </h2>
+
+    <h2 v-bind:class="['new', 'promoted']">Newly Pormoted Movies</h2>
+    <h2
+      v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'new' : 'sold-out']"
+    >
+      Array Conditional Movies
+    </h2>
+
+    <h2
+      v-bind:class="{
+        promoted: isPromoted,
+        new: !isSoldOut,
+        'sold-out': isSoldOut,
+      }"
+    >
+      Object Conditional Movies
+    </h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      greet: "Greetings",
+      name: "Learning VueJS",
+      channel: "<b>Coding</b>",
+      headingId: "heading",
+      isDisabled: true,
+      status: "danger",
+      isPromoted: false,
+      isSoldOut: false,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +66,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.danger {
+  color: red;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+.sold-out {
+  color: blue;
 }
 </style>
